b2517610ee7790701d66fc2d4c75fc650d15654e
CyberFights <shadowjer011@gmail.com>
2026-06-15 14:58:50 -0400

    Update dm-toggle.js

--- public/js/dm-toggle.js
+++ public/js/dm-toggle.js
@@ -1,4 +1,7 @@
 document.getElementById("btnDMs").addEventListener("click", () => {
   const panel = document.getElementById("dmSidebar");
+  const chat = document.getElementById("chatPopup");
+
   panel.classList.toggle("open");
+  chat.classList.toggle("shifted");
 });
