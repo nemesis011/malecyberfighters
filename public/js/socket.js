c2ab9527d66e14da7092034c455c4706d2373d89
CyberFights <shadowjer011@gmail.com>
2026-06-26 03:26:26 -0400

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -33,3 +33,12 @@ socket.on('privateMessage', pm => {
     if (window.updateDMListSidebar) updateDMListSidebar();
   }
 });
+socket.on("typingDM", ({ from }) => {
+  const el = document.getElementById("pmTyping_" + from);
+  if (el) el.style.display = "block";
+});
+
+socket.on("stopTypingDM", ({ from }) => {
+  const el = document.getElementById("pmTyping_" + from);
+  if (el) el.style.display = "none";
+});
