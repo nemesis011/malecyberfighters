76908b1e3f42c015482900608cc49befb647e798
CyberFights <shadowjer011@gmail.com>
2026-06-28 16:51:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const authLimiter = rateLimit({
