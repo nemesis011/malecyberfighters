4a687e7fb86b5572473fd5b0ab34fc0994f3735c
CyberFights <shadowjer011@gmail.com>
2026-07-13 20:40:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.get("*", (req, res, next) => {
+app.get("/", (req, res, next) => {
   const ua = req.headers["user-agent"] || "";
 
   const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(ua);
