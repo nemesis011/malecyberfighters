3fb28c30c3e9dc64c3dbdbed5844414c56c8fee2
CyberFights <shadowjer011@gmail.com>
2026-07-13 20:52:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,17 +50,7 @@ app.use(
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.get("/", (req, res, next) => {
-  const ua = req.headers["user-agent"] || "";
 
-  const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(ua);
-
-  if (isMobile) {
-    return res.redirect("https://malecyberfighters.base44.app");
-  }
-
-  next();
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
